f34f146a4ce9ed9abb80abe75ddc1daf667b3f07
Nick Chen <chenyahua2012@hotmail.com>
2020-05-31 17:00:21 -0400

    use goutte to get job list from indeed.com

--- index.php
+++ index.php
@@ -7,9 +7,27 @@
     <title>Scraper!</title>
 </head>
 <body>
+    <ul style="width:60%">
+        <?php
 
-    <?php
-        echo 'Hello world!!'
-    ?>
+            include ('vendor/autoload.php');
+
+            $client = new \Goutte\Client();
+            $crawler = $client->request('GET', 'https://www.indeed.com/jobs?q=frontend%20developer&l=New%20York%2C%20NY&jt=fulltime&sort=date&vjk=439761a71c037dc6');
+            $fullPageHtml = $crawler->html();
+
+            $pageH1 = $crawler->filter('.jobsearch-SerpJobCard')->each(function($node){
+
+                echo  '<li style="margin:20px 0"><div>'
+                         . $node->filter('.title')->text() . 
+                       '</div><div style="color:lightGrey">' . $node->filter('.sjcl')->text() . 
+                       '</div><div style="color:grey">' 
+                       . $node->filter('.summary')->text() .  
+                       '</div></li>' ;
+            })
+
+            
+        ?>
+    </ul>
 </body>
 </html>
