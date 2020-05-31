828f22fd8395a680e93fec8ad0f56d61f01809e7
Nick Chen <chenyahua2012@hotmail.com>
2020-05-31 17:26:56 -0400

    update

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
+<?php
+    $stringOne = 'Here is my email ';
+    $stringTwo = 'nchen2020@gmail.com';
+    echo $stringOne.$stringTwo;
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
-    <title>Scraper!</title>
+    <title>PHP to MySQL</title>
 </head>
 <body>
 
